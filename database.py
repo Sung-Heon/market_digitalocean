a4252d1b4ed4c1b5c6d0f7b19ab236275f10184b
elon <elon4856@gmail.com>
2022-08-20 16:54:51 +0900

    db 세팅

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = f'''jdbc:mysql://{os.environ['DB_ID']}:{os.environ['DB_PASSWORD']}@kurly-ss.c78egjhpmq4e.ap-northeast-2.rds.amazonaws.com:3306'''
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ Main FastAPI app.
 from typing import List
 import os
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
+from sqlalchemy.orm import Session
 
+import crud
+import schemas
+from database import SessionLocal
 from description import desc
 from helpers import emojis, passwd, random_users
 from pydantic_models import Emoji, Password, Person
@@ -18,6 +22,19 @@ app = FastAPI(
     version="0.0.1",
     docs_url="/",
 )
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/test/", response_model=List[schemas.Test])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tests = crud.get_items(db, skip=skip, limit=limit)
+    return tests
 
 
 @app.get("/person", response_model=List[Person], tags=["Randomness..."])
